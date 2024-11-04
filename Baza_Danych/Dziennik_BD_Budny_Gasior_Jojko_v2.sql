4896c139d739b3972c71042814431f35c957853b
Dominik Jojko <20dominik01@gmail.com>
2024-11-04 10:57:54 +0100

    Update Dziennik_BD_Budny_Gasior_Jojko_v2.sql

--- Baza_Danych/Dziennik_BD_Budny_Gasior_Jojko_v2.sql
+++ Baza_Danych/Dziennik_BD_Budny_Gasior_Jojko_v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Paź 24, 2024 at 03:16 PM
+-- Generation Time: Lis 04, 2024 at 10:57 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -40,9 +40,13 @@ CREATE TABLE `formy_sprawdzania` (
 --
 
 INSERT INTO `formy_sprawdzania` (`id`, `kurs_id`, `typ`, `opis`, `waga`) VALUES
-(5, 2, 'kartkowka', 'Pan Tadeusz', 0),
-(6, 2, 'sprawdzian', '', 1),
-(7, 2, 'odpowiedz_ustna', '', 0);
+(5, 2, 'kartkowka', 'Logarytmy', 2),
+(6, 2, 'sprawdzian', '', 3),
+(7, 2, 'odpowiedz_ustna', '', 1),
+(8, 2, 'sprawdzian', 'Matura', 5),
+(11, 2, 'kartkowka', 'Matura', 1),
+(12, 2, 'odpowiedz_ustna', 'Matura', 1),
+(14, 6, 'kartkowka', 'Wstęp', 3);
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,11 @@ CREATE TABLE `kursy` (
 --
 
 INSERT INTO `kursy` (`id`, `nauczyciel_id`, `nazwa`, `data_rozpoczecia`) VALUES
-(2, 1, 'Matematyka - PAW3', '2024-10-29');
+(2, 1, 'Matematyka - PAW3', '2024-10-29'),
+(3, 1, 'J.Polski - PAW 2', '2026-04-02'),
+(4, 1, 'Test', '2024-10-27'),
+(5, 1, 'Test5', '2024-11-20'),
+(6, 7, 'IAM2', '2024-10-30');
 
 -- --------------------------------------------------------
 
@@ -85,7 +93,9 @@ CREATE TABLE `nauczyciele` (
 INSERT INTO `nauczyciele` (`id`, `imie`, `nazwisko`, `login`, `haslo`) VALUES
 (1, 'Jarosław', 'Gąsior', 'jrygames', '$2b$10$D8.20ybPITSV9prMSECIQ.3T5MsjCfro2yocXEo9ws3gRCxnpxIwG'),
 (2, 'Adrian', 'Budny', 'abudny', '$2b$10$PvtEYA8b847l0.t0DGZZFerz0T.2rjz3z5OIe9vzpcA6g78jQHPy6'),
-(4, 'a', 'b', 'c', '$2b$10$PMtHrGU5XsKiF1vg6R0jIOLmlRx0IoOhgDB86NNHrh027B1IRTu/W');
+(4, 'a', 'b', 'c', '$2b$10$PMtHrGU5XsKiF1vg6R0jIOLmlRx0IoOhgDB86NNHrh027B1IRTu/W'),
+(6, 'b', 'b', 'b', '$2b$10$HDI0.4IiEYRNWTYJhemBlu.brQBuq1qPtWegSsGV5q/84K4EHlf/q'),
+(7, 'abc', 'abc', 'abc', '$2b$10$D1S2YoYmL9d.j7GpPj5hqO2VdUjGa9qUtSz2NR9FsOwYy7xy0G3iS');
 
 -- --------------------------------------------------------
 
@@ -107,7 +117,15 @@ CREATE TABLE `obecnosci` (
 
 INSERT INTO `obecnosci` (`id`, `kurs_id`, `uczen_id`, `data`, `status`) VALUES
 (7, 2, 2, '2024-10-29', NULL),
-(8, 2, 4, '2024-10-31', NULL);
+(8, 2, 4, '2024-10-29', NULL),
+(13, 2, 3, '2024-10-29', 1),
+(37, 2, 2, '2024-10-28', NULL),
+(38, 2, 3, '2024-10-28', NULL),
+(39, 2, 4, '2024-10-28', NULL),
+(40, 6, 2, '2024-10-30', 1),
+(41, 6, 4, '2024-10-30', 1),
+(42, 6, 2, '2024-10-29', 0),
+(43, 6, 4, '2024-10-29', 0);
 
 -- --------------------------------------------------------
 
@@ -129,10 +147,18 @@ CREATE TABLE `oceny` (
 --
 
 INSERT INTO `oceny` (`id`, `uczen_id`, `forma_sprawdzania_id`, `wartosc`, `data`, `anulowana`) VALUES
-(1, 2, 5, 4.00, '2024-10-29', 0),
+(1, 2, 5, 1.00, '2024-10-29', 0),
 (2, 4, 6, 3.00, '2024-10-31', 0),
 (3, 4, 5, 5.00, '2024-10-24', 0),
-(4, 2, 6, 5.00, '2024-10-24', 0);
+(4, 2, 6, 5.00, '2024-10-24', 0),
+(5, 2, 7, 2.00, '2024-10-24', 0),
+(6, 2, 8, 3.00, '2024-10-24', 0),
+(7, 4, 8, 5.00, '2024-10-24', 0),
+(8, 4, 7, 4.00, '2024-10-24', 0),
+(9, 2, 11, 1.00, '2024-10-24', 0),
+(10, 4, 11, 5.00, '2024-10-24', 0),
+(13, 2, 14, 4.00, '2024-10-29', 0),
+(14, 4, 14, 3.00, '2024-10-29', 0);
 
 -- --------------------------------------------------------
 
@@ -153,7 +179,9 @@ CREATE TABLE `uczniowie` (
 INSERT INTO `uczniowie` (`id`, `imie`, `nazwisko`) VALUES
 (2, 'Adrian', 'Budny'),
 (3, 'Jarosław', 'Gąsior'),
-(4, 'Dominik', 'Jojko');
+(4, 'Dominik', 'Jojko'),
+(33, 'Jarasław', 'Gąsior'),
+(35, 'abc', 'abc');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -210,37 +238,37 @@ ALTER TABLE `uczniowie`
 -- AUTO_INCREMENT for table `formy_sprawdzania`
 --
 ALTER TABLE `formy_sprawdzania`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `kursy`
 --
 ALTER TABLE `kursy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `nauczyciele`
 --
 ALTER TABLE `nauczyciele`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `obecnosci`
 --
 ALTER TABLE `obecnosci`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `oceny`
 --
 ALTER TABLE `oceny`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `uczniowie`
 --
 ALTER TABLE `uczniowie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- Constraints for dumped tables
